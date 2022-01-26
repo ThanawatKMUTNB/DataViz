03d44aa205c8e3a34f40e53a4b328949f67e9284
Thanawat Thongpia <fairforwork@hotmail.com>
2022-01-26 22:13:12 +0700

    test Graph Profit with Region

---
+++ Graph.py
@@ -0,0 +1,65 @@
+import sys, random
+from PyQt5.QtWidgets import (QApplication, QMainWindow)
+from PyQt5.QtChart import QChart, QChartView, QValueAxis, QBarCategoryAxis, QBarSet, QBarSeries
+from PyQt5.Qt import Qt
+from PyQt5.QtGui import QPainter
+import pandas as pd
+
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.resize(800, 600)
+
+
+		df = pd.read_csv('Superstore.csv', encoding='windows-1252')
+		Reg = []
+		for i in df['Region'].values:
+			if i not in Reg:
+				Reg.append(i)
+
+		df.set_index('Region',inplace=True)
+		profit = []
+		for i in Reg:
+			profit.append(sum(df.loc[i,'Profit']))
+
+
+		set0 = QBarSet('Profit')
+
+
+		set0.append(profit)
+
+
+		series = QBarSeries()
+		series.append(set0)
+
+
+		chart = QChart()
+		chart.addSeries(series)
+		chart.setTitle('Bar Chart Demo')
+		chart.setAnimationOptions(QChart.SeriesAnimations)
+
+		months = ('test1')
+		months = tuple(Reg)
+
+		axisX = QBarCategoryAxis()
+		axisX.append(months)
+
+		axisY = QValueAxis()
+		axisY.setRange(0, max(profit))
+
+		chart.addAxis(axisX, Qt.AlignBottom)
+		chart.addAxis(axisY, Qt.AlignLeft)
+
+		chart.legend().setVisible(True)
+		chart.legend().setAlignment(Qt.AlignBottom)
+
+		chartView = QChartView(chart)
+		self.setCentralWidget(chartView)
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+
+	window = MainWindow()
+	window.show()
+
+	sys.exit(app.exec_())
