74c5cf0b519948649ba884514170376254b6d079
tatazakup <s6301012620022@email.kmutnb.ac.th>
2022-02-01 04:07:47 +0700

    MainGUI By Cookie

--- mPageByCookie.py
+++ mPageByCookie.py
@@ -1,34 +1,137 @@
+import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+import numpy as np
+import pandas as pd
+import csvManager
+#from qgis.PyQt.QtWidgets import QVBoxLayout
 
+class TableModel(QtCore.QAbstractTableModel):
+    data = None
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        #self.itemClicked.connect(self.handleItemClick)
+        self._data = data
+        
 
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            value = self._data.iloc[index.row(), index.column()]
+            return str(value)
+
+    def rowCount(self, index):
+        return self._data.shape[0]
+
+    def columnCount(self, index):
+        return self._data.shape[1]
+
+    def headerData(self, section, orientation, role): #show Header on column
+        # section is the index of the column/row.
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal: #x
+                return self._data.columns[section]
+
+            '''if orientation == Qt.Vertical: #y
+                return ''.join(self._data.index[section])'''
 class Ui_MainWindow(object):
+    
+    def dataSource(self):
+        self.data = csvManager.getDataWithPandas()
+
+    def dataSourceSort(self,dimention):
+        self.data = csvManager.setAllDataByOneDimention(dimention)
+        
+    def sheetPageRow(self,dimention):
+        self.data = csvManager.setDimentionSort(dimention)
+        self.data[" "] = "abc"
+                
+    def sheetPageCol(self,dimention):
+        tmp = csvManager.setDimentionSort(dimention)
+        tmp[" "] = "abc"
+        self.data = tmp.T
+        
+    def sheetPageAddCol(self,Row,Col):
+        tmp = csvManager.getDataWithPandasByHead(Col)
+        tmp = tmp.sort_values(by=Col)
+        tmp = tmp.drop_duplicates().values
+        res = list(map(list, zip(*tmp)))
+        print(res)
+        newDf = []
+        for i in res:
+            newDf.append(Row+i)
+        self.data.columns = tuple(newDf[-1])
+        df = pd.DataFrame(newDf)
+        
+    def sheetPageAddRow(self,Row):
+        lists = [list(x) for x in self.data.index]
+        print(len(lists))
+        subRow = np.array(lists).T.tolist()
+        for i,j in zip(reversed(subRow),Row):
+            self.data.insert(0,j,i)
+        
+    def sheetPageRowAndCol(self,Row,Col):
+        if len(set(Col)) == 0: MainWindow.sheetPageRow(self,Row)
+        elif len(set(Row)) == 0: MainWindow.sheetPageCol(self,Col)
+        else : 
+            self.data = csvManager.setRowAndColumn(Row,Col)
+            MainWindow.sheetPageAddRow(self,Row)
+            MainWindow.sheetPageAddCol(self,Row,Col)
+    
+    def handleSelectionChanged(self, selected, deselected):
+        for index in self.table.selectionModel().selectedRows():
+            print('Row %d is selected' % index.row())
+            
     def setupUi(self, MainWindow):
+        
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
+        
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
+        
         self.tabWidget.setGeometry(QtCore.QRect(4, 0, 791, 571))
         self.tabWidget.setObjectName("tabWidget")
         self.tab = QtWidgets.QWidget()
         self.tab.setObjectName("tab")
+        
+        '''data = csvManager.getDataWithPandas()
+        dataCol = csvManager.getHead()
+        #print(len(data))
         self.DataSource_2 = QtWidgets.QTableWidget(self.tab)
         self.DataSource_2.setGeometry(QtCore.QRect(190, 10, 581, 501))
         self.DataSource_2.setObjectName("DataSource_2")
-        self.DataSource_2.setColumnCount(3)
-        self.DataSource_2.setRowCount(3)
-        item = QtWidgets.QTableWidgetItem()
-        self.DataSource_2.setVerticalHeaderItem(0, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.DataSource_2.setVerticalHeaderItem(1, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.DataSource_2.setVerticalHeaderItem(2, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.DataSource_2.setHorizontalHeaderItem(0, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.DataSource_2.setHorizontalHeaderItem(1, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.DataSource_2.setHorizontalHeaderItem(2, item)
+        self.DataSource_2.setColumnCount(len(dataCol))
+        self.DataSource_2.setRowCount(len(data))'''
+        
+        self.table = QtWidgets.QTableView(self.tab)
+        self.table.setGeometry(QtCore.QRect(190, 10, 581, 501))
+        Ui_MainWindow.dataSource(self)
+        self.model = TableModel(self.data)
+        '''model =  QtGui.QStandardItemModel(len(self.data), len(self.data.columns), self.table)
+        for row in range(len(self.data)):
+            for column in range(len(self.data.columns)):
+                item = QtGui.QStandardItem('(%d, %d)' % (row, column))
+                item.setTextAlignment(QtCore.Qt.AlignCenter)
+                model.setItem(row, column, item)'''
+        self.table.setModel(self.model)
+        '''selection = self.table.selectionModel()
+        selection.selectionChanged.connect(self.handleSelectionChanged)'''
+        #layout = QtGui.QVBoxLayout(self)
+        #layout.addWidget(self.table)
+        
+        '''for i,r in zip(range(len(dataCol)),dataCol):
+            #print(i,r)
+            item = QtWidgets.QTableWidgetItem()
+            self.DataSource_2.setHorizontalHeaderItem(i, item)
+            
+        for i,r in zip(range(len(data)),list(data)):
+            #print(i,r)
+            item = QtWidgets.QTableWidgetItem()
+            self.DataSource_2.setVerticalHeaderItem(i, item)'''
+        
+        #self.DataSource_2.setItem(0, 0, item)
         self.listView = QtWidgets.QListView(self.tab)
         self.listView.setGeometry(QtCore.QRect(10, 10, 171, 271))
         self.listView.setObjectName("listView")
@@ -110,6 +213,7 @@ class Ui_MainWindow(object):
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(3, item)
         self.tabWidget.addTab(self.tab_2, "")
+        
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
@@ -126,18 +230,46 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        item = self.DataSource_2.verticalHeaderItem(0)
+        '''data = csvManager.getDataWithPandas()
+        dataCol = csvManager.getHead()
+        #print(dataCol)
+        #print(list(data["Row ID"]))
+        
+        for i,r in zip(range(len(dataCol)),dataCol):
+            #print(i,r)
+            item = self.DataSource_2.horizontalHeaderItem(i) 
+            item.setText(_translate("MainWindow", str(r)))
+            #for j in range(len(data)):
+            
+        for i,r in zip(range(len(list(data))),list(data)):
+            #print(i,r)
+            item = self.DataSource_2.verticalHeaderItem(i)
+            #print(item)
+            #item.setText(_translate("MainWindow", "kkk"))'''
+        
+            
+        '''item = self.DataSource_2.verticalHeaderItem(0)
         item.setText(_translate("MainWindow", "0"))
         item = self.DataSource_2.verticalHeaderItem(1)
         item.setText(_translate("MainWindow", "1"))
         item = self.DataSource_2.verticalHeaderItem(2)
-        item.setText(_translate("MainWindow", "2"))
-        item = self.DataSource_2.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "2"))'''
+        
+        '''item = self.DataSource_2.horizontalHeaderItem(0)
         item.setText(_translate("MainWindow", "0"))
         item = self.DataSource_2.horizontalHeaderItem(1)
         item.setText(_translate("MainWindow", "1"))
         item = self.DataSource_2.horizontalHeaderItem(2)
-        item.setText(_translate("MainWindow", "2"))
+        item.setText(_translate("MainWindow", "2"))'''
+        
+        #__sortingEnabled = self.DataSource_2.isSortingEnabled()
+        #self.DataSource_2.setSortingEnabled(False)
+        
+        #item = self.DataSource_2.item(0, 0)
+        #item.setText(_translate("MainWindow", "kkk"))
+        
+        #self.DataSource_2.setSortingEnabled(__sortingEnabled)
+        
         self.pushButton.setText(_translate("MainWindow", "PushButton"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Tab 1"))
         item = self.tableWidget_2.horizontalHeaderItem(0)
