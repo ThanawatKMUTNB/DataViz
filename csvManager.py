0cd1d68f303a33aa99153c6080137fa232e83388
PoonPooncharat <88476531+PoonPooncharat@users.noreply.github.com>
2022-02-09 17:57:23 +0700

    Update csvManager.py

--- csvManager.py
+++ csvManager.py
@@ -2,7 +2,6 @@ from itertools import chain
 from re import S
 import numpy as np
 import pandas as pd
-import main as m
 #import mPageByCookie
 class csvManager:
     def __init__(self):
@@ -17,6 +16,7 @@ class csvManager:
         fileExtension = path.split(".")
         if fileExtension[-1] == "csv":
             df = pd.read_csv(path, encoding='windows-1252')
+            
         else:
             #print(fileExtension[-1])
             df = pd.read_excel(path, engine = "openpyxl")
@@ -29,7 +29,7 @@ class csvManager:
         return self.df
 
     def getDataWithPandasByHead(self,head):
-        #print(head)
+        print(head)
         return self.df[head]
 
     def getAxisYName(self,Dimension):
@@ -68,28 +68,14 @@ class csvManager:
         return new
 
     def setDimensionSort(self,Dimension):
-        if len(Dimension) == len(set(Dimension)):
-            sortedData = self.getDataWithPandasByHead(Dimension)
-            #print(sortedData)
-            new = sortedData.sort_values(by=Dimension)
-        else:
-            sortedData = None
-            dflist = []
-            dflistdup = []
-            for i in Dimension:
-                if i not in dflist:
-                    dflist.append(i)
-                    sortedData = self.getDataWithPandasByHead(dflist)
-                    sortedData = sortedData.sort_values(by=dflist)
-                    #print("---",sortedData.loc[:,sortedData.columns[-1]])
-                else:
-                    #print(pd.DataFrame({i:sortedData[i]}))
-                    dflistdup.append([len(dflist),pd.DataFrame({i:sortedData[i]})])
-            for i in dflistdup:
-                tmp = i[1]
-                sortedData.insert(i[0], tmp.columns[-1] ,tmp.loc[:,tmp.columns[-1]],allow_duplicates=True)
-            new = sortedData
-        #print(new)
+        sortedData = self.getDataWithPandasByHead(Dimension)
+        #print(sortedData)
+        #print(Dimension)
+        new = sortedData.sort_values(by=Dimension)
+        #new.set_index([Dimension[0]])
+        #new[''] = pd.Series("abc", index=new.index)
+        #print("---------",new)
+        pd.MultiIndex.from_frame(new)
         return new
 
     def getMeasure(self):
@@ -124,8 +110,10 @@ class csvManager:
             df = self.readFile(self.path+"/"+i)
             li.append(df)
         frame = pd.concat(li, axis=0, ignore_index=True)
-        frame.sort_values("Row ID", inplace = True)
-        frame.drop_duplicates(inplace=True)
+        #frame.sort_values("Row ID", inplace = True)
+
+        ##############################################
+        frame.drop_duplicates(inplace= True) #delete same data
         self.df = frame
         return self.df
         
@@ -140,76 +128,16 @@ class csvManager:
             if i not in tmp:
                 tmp.append(i)
         return len(tmp)
-    Measure = ['Sales', 'Quantity', 'Discount', 'Profit']
+
     def setRowAndColumn(self,Row,Col):
-        isInterRow = list(set.intersection(set(Row),set(self.Measure)))
-        isInterCol = list(set.intersection(set(Col),set(self.Measure)))
-        #print(isInterRow,isInterCol)
-        if isInterRow == [] and isInterCol == []:
-            rowList = self.getDataWithPandasByHead(Row)
-            colList = self.getDataWithPandasByHead(Col)
-            results = pd.concat([rowList, colList], axis=1,ignore_index=True)
-            results[" "] = "abc"
-            results = results.sort_values(by=results.columns.tolist())
-            results = results.drop_duplicates()
-            k = results.pivot(results.columns[len(Row):-1].tolist(),results.columns[:len(Row)].tolist())
-            k = k.replace(np.nan, '')
-            k = k.T
-            #print(k.T)
-        else:
-            filterChoose = "sum"
-            if isInterRow != []:
-                for i in isInterRow:
-                    Row.remove(i)
-                intersec = isInterRow
-            else:
-                for i in isInterCol:
-                    Col.remove(i)
-                intersec = isInterCol
-            rowList = self.getDataWithPandasByHead(Row)
-            colList = self.getDataWithPandasByHead(Col+intersec)
-            #DiList = self.getDataWithPandasByHead(intersec)
-            results = pd.concat([rowList, colList], axis=1,ignore_index=True)
-            results = results.sort_values(by=results.columns.tolist())
-            #results = results.drop_duplicates()
-            #print(intersec)
-            #print(results)
-            #print(results)
-            colNum = results.columns.tolist()
-            beforMesual = (-1)*len(intersec)
-            '''DiList = results.groupby(colNum[:beforMesual])[colNum[beforMesual:]].sum()
-            print(DiList)'''
-            #print("-----------",colNum[beforMesual:])
-            if isInterRow != []:
-                k = pd.pivot_table(results,index = colNum[len(Row):beforMesual], columns = colNum[:len(Row)],values = colNum[beforMesual:],aggfunc=np.sum)
-                k = k.round(0)
-                k=k.T
-                #print(k.index.names)
-                k.columns.names = Col
-                k.index.names = [None]+Row
-            else:
-                k = pd.pivot_table(results,columns = colNum[len(Row):beforMesual], index= colNum[:len(Row)],values = colNum[beforMesual:],aggfunc=np.sum)
-                k = k.round(0)
-                #print(k.values)
-                k.columns.names = [None]+Col
-                k.index.names = Row
-            k = k.replace(np.nan, '')
-        #print(k)
-        '''tmp = [list(ele) for ele in k.index]
-        eachList = []
-        for j in range(len(tmp[0])):
-            eachList.append([i[j] for i in tmp[:]])
-        for i in range(len(eachList)):
-            for j in range(len(eachList[i])-1,0,-1):
-                if eachList[i][j] == eachList[i][j-1] :
-                    eachList[i][j]=''
-        changIndex = pd.MultiIndex.from_arrays(eachList, names=k.index.names)
-        k.index = changIndex
-        print(k)'''
+        #self.df = pd.read_csv("SS_20lines.csv", encoding='windows-1252')
+        baseList = self.setDimensionSort(list(set(Row+Col)))
+        baseList[" "] = "abc"
+        k = pd.pivot(baseList, values=[" "], index=Row,columns=Col)
+        k = k.replace(np.nan, '', regex=True)
+        k.drop_duplicates()
+        print(k)
         return k
 
-'''ex = csvManager()
-ex.df = pd.read_csv("Superstore.csv", encoding='windows-1252')
-#ex.df = pd.read_csv("SS_20lines.csv", encoding='windows-1252')
-#ex.setDimensionSort(["Region","Segment","Region","Region"])
-ex.setRowAndColumn(["Region","Region","Segment"],["Region","Sales","Profit"])'''
+#ex = csvManager()
+#ex.setRowAndColumn(["Segment","Region"],["Ship Mode","City"])
